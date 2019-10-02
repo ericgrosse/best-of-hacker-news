3a424854b3708c735245dfd1c74f6ab2c9a92296
ericgrosse <ericgrosse1@gmail.com>
2019-10-02 16:30:57 -0400

    added SASS support, hot module reloading and cleaned up boilerplate code

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root')
+
+ReactDOM.render(<App />, rootEl);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Enable hot module reloading without ejecting
+if ((module as any).hot) {
+  (module as any).hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    )
+  })
+}

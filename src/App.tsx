e9aa4841b381fe1b84a6531d51483649889333e3
Eric Grosse <ericgrosse1@gmail.com>
2019-10-03 21:10:03 +0000

    Added Grid for layout, also corrected formatting issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.scss';
-import axios from 'axios'
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
 
-interface Props {}
+interface Props { }
 
 interface State {
   hackerNewsData: Array<Story>
@@ -24,49 +25,49 @@ class App extends React.Component<Props, State> {
     // Retrieve the top 20 storyIDs
     const { data } = await axios.get(`https://hacker-news.firebaseio.com/v0/topstories.json`);
     let storyIDs = new Array(20).fill(null);
-    
+
     for (let i = 0; i < Math.min(data.length, storyIDs.length); ++i) {
       storyIDs[i] = data[i];
     }
-    
+
     // hackerNewsData is an array of objects, each containing story data and an array of top 10 comments
     const hackerNewsData = await Promise.all(
       // Retrieve the stories and top 10 commentIDs for each story
-      storyIDs.map(async (storyID : number) => {
+      storyIDs.map(async (storyID: number) => {
         try {
           const { data } = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${storyID}.json`);
           const commentIDs = new Array(10).fill(null);
-  
+
           for (let i = 0; i < Math.min((data.kids || []).length, commentIDs.length); ++i) {
             commentIDs[i] = data.kids[i];
           }
-  
+
           // Retrieve the comments from the commentIDs
           const comments = await Promise.all(
-            commentIDs.map(async (commentID : number) => {
+            commentIDs.map(async (commentID: number) => {
               try {
                 const { data } = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${commentID}.json`);
                 return (data || {}).text || null;
-              } catch(e) {
+              } catch (e) {
                 console.error(e);
                 return null;
               }
             })
           );
-  
+
           return {
             ...data,
             comments,
           };
         }
-        catch(e) {
+        catch (e) {
           console.error(e);
           return {};
         }
       })
     );
 
-    this.setState({hackerNewsData})
+    this.setState({ hackerNewsData })
   }
 
   render() {
@@ -74,26 +75,31 @@ class App extends React.Component<Props, State> {
 
     return (
       <div className="App">
-        <h1>Best of Hacker News</h1>
-        {
-          state.hackerNewsData.map((story, index) => {
-            return (
-              <div key={index}>
-                <h5><a href={story.url}>{story.title}</a></h5>
-                <h6>By: {story.by}</h6>
-                {
-                  story.comments.map((comment, index) => {
-                    return (
-                      <div key={index}>
-                        <div dangerouslySetInnerHTML={{ __html: comment }} />
-                      </div>
-                    )
-                  })
-                }
-              </div>
-            )
-          })
-        }
+        <Grid container justify={'center'}>
+          <Grid item xs={12} sm={6}>
+            <h1>Best of Hacker News</h1>
+            {
+              state.hackerNewsData.map((story, index) => {
+                return (
+                  <div key={index}>
+                    <h5><a href={story.url}>{story.title}</a></h5>
+                    <h6>By: {story.by}</h6>
+                    {
+                      story.comments.map((comment, index) => {
+                        return (
+                          <div key={index}>
+                            <div dangerouslySetInnerHTML={{ __html: comment }} />
+                          </div>
+                        )
+                      })
+                    }
+                  </div>
+                )
+              })
+            }
+          </Grid>
+
+        </Grid>
       </div>
     );
   }
